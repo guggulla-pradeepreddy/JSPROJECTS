fb0b06a77b712b39dff0f805313a2dc3b47c37ee
guggulla-pradeepreddy <pradeep.gv230493@gmail.com>
2015-11-30 18:10:08 +0530

    initial commit

---
+++ JSPROJECTS/registrationform.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JSPROJECTS
+{
+    public partial class registrationform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
